7d35e7ad2c46789f0590d58d8f1118b90472a560
Vu Khanh <nguyenvukhanh0202@gmail.com>
2022-12-15 17:09:53 +0700

    Update footer

--- src/components/Sections/Footer.jsx
+++ src/components/Sections/Footer.jsx
@@ -17,16 +17,13 @@ export default function Contact() {
           <InnerWrapper className="flexSpaceCenter" style={{ padding: "30px 0" }}>
             <Link className="flexCenter animate pointer" to="home" smooth={true} offset={-80}>
               <LogoImg />
-              <h1 className="font15 extraBold whiteColor" style={{ marginLeft: "15px" }}>
-                Fanatic
-              </h1>
             </Link>
             <StyleP className="whiteColor font13">
-              © {getCurrentYear()} - <span className="purpleColor font13">Fanatic</span> All Right Reserved
+              © {getCurrentYear()} - All Right Reserved
             </StyleP>
 
             <Link className="whiteColor animate pointer font13" to="home" smooth={true} offset={-80}>
-              Back to top
+              Lên Đầu Trang
             </Link>
           </InnerWrapper>
         </div>
