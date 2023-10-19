de810ab15a7ab709f6dc9d67c6105b575c1de79e
vukhanh.nguyen <nguyenvukhanh0202@gmail.com>
2023-10-19 20:30:34 +0700

    Minor update

--- src/screens/Landing.jsx
+++ src/screens/Landing.jsx
@@ -16,7 +16,7 @@ export default function Landing() {
       <Projects />
       {/* <Blog /> */}
       <Pricing />
-      <div style={{ margin: "1rem auto" }}>
+      <div style={{ margin: "1rem auto", textAlign: 'center' }}>
         <iframe
           title="Address"
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d240.01708377412234!2d108.35153367666183!3d15.736672789902036!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31420a0a0a46eed7%3A0x90cec496d1df65ed!2zNDQgTmd1eeG7hW4gVsSDbiBUcuG7l2ksIFRULiBIw6AgTGFtLCBUaMSDbmcgQsOsbmgsIFF14bqjbmcgTmFtLCBWaWV0bmFt!5e0!3m2!1sen!2s!4v1697721608182!5m2!1sen!2s"
